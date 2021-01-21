7c147adb432d9eb34294cdd2a39c6445b4db1cc0
WangWeiQing <wang.weiqing@qq.com>
2021-01-21 14:39:44 +0800

    用户页面

--- pages/user/user.js
+++ pages/user/user.js
@@ -5,14 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    baseAddress: "http://localhost:8080/",
+    baseImageAddress: "http://localhost:8080/img/",
+    user: [],
+    body: [],
+    /**
+     * 从用户的登录界面获取
+     * TODO
+     */
+    userId: '28adf861-5b81-11eb-8fb9-00163e10d555'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getUserById();
+    this.getBodyById();
+    
   },
 
   /**
@@ -62,5 +72,48 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 通过id获取用户信息
+   */
+  getUserById: function(){
+    var that = this;
+
+    wx.request({
+      url: that.data.baseAddress + 'user/selectOne?id=' + that.data.userId,
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          user: res.data.data
+        });
+      }
+    });
+  },
+
+  /**
+   * 根据id获取身体信息
+   */
+  getBodyById: function () {
+    var that = this;
+
+    wx.request({
+      url: that.data.baseAddress + 'body/selectOne?id=' + that.data.userId,
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        that.setData({
+          body: res.data.data
+        });
+      }
+    });
+  },
+
+  
+  getCalendarData(e) { // 监听日历数据
+    console.log(e.detail)
   }
 })
